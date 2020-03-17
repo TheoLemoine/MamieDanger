c9375c3f46e4dc364087c13f22abf7156837c9e8
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-03-17 18:19:29 +0100

    :art: Changed clip shader when player is behind wall

---
+++ Assets/Scripts/Terrain/SendPositionToShader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendPositionToShader : MonoBehaviour
+{
+    [SerializeField] private Material material;
+    [SerializeField] private string propKey;
+    private Vector4 _posVector = new Vector4();
+    private Transform _transform;
+
+    private void Start()
+    {
+        _transform = transform;
+        UpdatePosVector();
+    }
+
+    private void UpdatePosVector()
+    {
+        Vector3 p = _transform.position;
+        _posVector.Set(p.x, p.y, p.z, 0);
+    }
+
+    private void Update()
+    {
+        UpdatePosVector();
+        material.SetVector(propKey, _posVector);
+    }
+}
