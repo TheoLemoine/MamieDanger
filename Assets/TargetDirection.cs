cffc71197f577415a69b537e99893536192627e0
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-03-26 14:38:27 +0100

    :art: Improved direction hint

--- Assets/TargetDirection.cs
+++ Assets/TargetDirection.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 
 public class TargetDirection : MonoBehaviour
 {
-    [SerializeField] private Transform playerTransform;
     [SerializeField] private Transform targetTransform;
-    [SerializeField] private float distance = 50f;
+    [SerializeField] [Range(0f, 200f)] private float margin = 30f;
     private Camera _camera;
     private RectTransform _indicatorTransform;
 
@@ -19,15 +18,41 @@ public class TargetDirection : MonoBehaviour
 
     private void Update()
     {
-        var angle = CalcAngle();
-        var newPos = new Vector3((float)Math.Cos(angle) * distance, (float)Math.Sin(angle) * distance, 0);
+        var screenSize = new Vector2(Screen.width, Screen.height);
+        var screenCenter = screenSize / 2f;
+
+        var targetScreenPoint = (Vector2)_camera.WorldToScreenPoint(targetTransform.position);
+        var cursorPoint = targetScreenPoint - screenCenter;
+
+        var cursorBox = screenSize - Vector2.one * (margin * 2f);
+        var cursorBoxCenter = cursorBox / 2f;
+
+        if (targetScreenPoint.x > margin && targetScreenPoint.x < cursorBox.x + margin
+         && targetScreenPoint.y < cursorBox.y + margin && targetScreenPoint.y > margin)
+        {
+            _indicatorTransform.localPosition = cursorPoint;
+            return;
+        }
+
+        var angle = Mathf.Atan2(cursorPoint.y, cursorPoint.x);
+        var cornerAngle = Mathf.Atan2(cursorBoxCenter.x, cursorBoxCenter.y);
+        
+        var newPos = Vector3.zero;
+        // Right
+        if (Mathf.Abs(angle) < Mathf.PI / 2f - cornerAngle)
+            newPos = new Vector2(cursorBoxCenter.x, Mathf.Tan(angle) * cursorBoxCenter.x);
+        // Top
+        else if (angle > Mathf.PI / 2f - cornerAngle && angle < Mathf.PI / 2f + cornerAngle)
+            newPos = new Vector2(Mathf.Tan(angle - Mathf.PI / 2f) * -cursorBoxCenter.y, cursorBoxCenter.y);
+        // Left
+        else if (Mathf.Abs(angle) > Mathf.PI / 2f + cornerAngle)
+            newPos = new Vector2(-cursorBoxCenter.x, Mathf.Tan(angle) * -cursorBoxCenter.x);
+        // Bottom
+        else if (angle > -Mathf.PI / 2f - cornerAngle && angle < -Mathf.PI / 2f + cornerAngle)
+            newPos = new Vector2(Mathf.Tan(angle - Mathf.PI / 2f) * cursorBoxCenter.y, -cursorBoxCenter.y);
+        
         _indicatorTransform.localPosition = newPos;
+        _indicatorTransform.localRotation = Quaternion.Euler(0,0, angle * Mathf.Rad2Deg);
     }
 
-    private float CalcAngle()
-    {
-        var targetScreenPoint = _camera.WorldToScreenPoint(targetTransform.position);
-        var cursorPoint = new Vector2(targetScreenPoint.x - Screen.width / 2f, targetScreenPoint.y - Screen.height / 2f);
-        return (float)Math.Atan2(cursorPoint.y, cursorPoint.x);
-    }
 }
