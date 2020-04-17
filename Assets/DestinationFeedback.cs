b497a6ac7b93cd64ffa5d2308b441425beb88d46
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-04-17 18:57:50 +0200

    :sparkles: Add destination feedback

---
+++ Assets/DestinationFeedback.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestinationFeedback : MonoBehaviour
+{
+    [SerializeField] private string durationPropCode;
+    [SerializeField] private string startTimePropCode;
+    private MeshRenderer _meshRenderer;
+    private Transform _transform;
+    private float _duration;
+    private int _startTimePropId;
+    
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _meshRenderer.enabled = false;
+        _transform = transform;
+        _startTimePropId = Shader.PropertyToID(startTimePropCode);
+        _duration = _meshRenderer.material.GetFloat(Shader.PropertyToID(durationPropCode));
+    }
+
+    public void OnSetDestination(Vector3 pos, Vector3 normal)
+    {
+        _meshRenderer.enabled = true;
+        _transform.position = pos + normal * 0.01f;
+        _transform.rotation = Quaternion.LookRotation(-normal, Vector3.up);
+        _meshRenderer.material.SetFloat(_startTimePropId, Time.time);
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.AI;
 using Global.Input;
+using UnityEngine.Events;
 using Utils;
 
 namespace Player
 {
+    [Serializable]
+    class ChangeDestinationEvent : UnityEvent<Vector3, Vector3>
+    {}
+    
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private SingleUnityLayer groundLayer;
+        [SerializeField] private ChangeDestinationEvent changeDestinationEvent;
         private NavMeshAgent _agent;
         private Transform _transform;
 
         private void Start()
         {
+            if (changeDestinationEvent == null)
+                changeDestinationEvent = new ChangeDestinationEvent();
+            
             _transform = transform;
             _agent = GetComponent<NavMeshAgent>();
             ListenInputs();
@@ -20,6 +30,7 @@ namespace Player
         
         private void Move(RaycastHit hit)
         {
+            changeDestinationEvent.Invoke(hit.point, hit.normal);
             _agent.SetDestination(hit.point);
         }
 
