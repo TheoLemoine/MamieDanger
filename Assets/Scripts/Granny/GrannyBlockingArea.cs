e2318bb94bd9fcd2bae90a34e0c3688ffdb848b3
Theo <theo.lemoine0@gmail.com>
2020-04-20 19:45:47 +0200

    :circus_tent: Added loads of prefabs

--- Assets/Scripts/Granny/GrannyBlockingArea.cs
+++ Assets/Scripts/Granny/GrannyBlockingArea.cs
@@ -1,23 +1,15 @@
-﻿using Granny;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class GrannyBlockingArea : MonoBehaviour
+namespace Granny
 {
-    [SerializeField] private GameObject granny;
-
-    private int _grannyId;
-    private GrannyController _grannyController;
-    
-    // Start is called before the first frame update
-    void Start()
+    public class GrannyBlockingArea : MonoBehaviour
     {
-        _grannyId = granny.GetInstanceID();
-        _grannyController = granny.GetComponent<GrannyController>();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.GetInstanceID() == _grannyId)
-            _grannyController.StopInteraction();
+        private void OnTriggerEnter(Collider other)
+        {
+            if (TryGetComponent<GrannyController>(out var grannyController))
+            {
+                grannyController.StopInteraction();
+            }  
+        }
     }
 }
