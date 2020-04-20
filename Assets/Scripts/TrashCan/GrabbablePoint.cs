1db029132451e7977076d02ea629e06029e03d5f
Theo <theo.lemoine0@gmail.com>
2020-04-20 12:16:35 +0200

    :sparkles: Prototype of dragable trashcan

---
+++ Assets/Scripts/TrashCan/GrabbablePoint.cs
@@ -0,0 +1,37 @@
+using System;
+using Abstract;
+using Interfaces;
+using UnityEngine;
+
+namespace TrashCan
+{
+    public class GrabbablePoint : AInteracToggleBehaviour
+    {
+        [SerializeField] private SpringJoint joint;
+        [SerializeField] private Rigidbody rigidbody;
+
+        private float _baseSpring;
+
+        private void Start()
+        {
+            _baseSpring = joint.spring;
+            joint.spring = 0;
+        }
+
+        protected override void InteractStart(IInteractor interactor)
+        {
+            joint.connectedBody = interactor.GetGameObject().GetComponent<Rigidbody>();
+            joint.spring = _baseSpring;
+            
+            rigidbody.constraints = RigidbodyConstraints.FreezeRotationZ;
+        }
+
+        protected override void InteractStop()
+        {
+            joint.connectedBody = null;
+            joint.spring = 0;
+            
+            rigidbody.constraints = RigidbodyConstraints.None;
+        }
+    }
+}
