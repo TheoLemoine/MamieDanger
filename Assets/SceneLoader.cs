d060d9b845cbd619375a68b3d2cc5e2167088f4f
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-04-22 18:16:12 +0200

    :package: Add text mesh pro essentials and created scene loader

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void GoTo(string sceneName)
+    {
+        UnitySceneManager.LoadScene(sceneName);
+    }
+}
