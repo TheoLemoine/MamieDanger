39b7cbb572d56f614ab2fd0da7f535544b269458
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-04-24 09:24:02 +0200

    :lipstick: Add ui to level 1

---
+++ Assets/EventAfterTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventAfterTime : MonoBehaviour
+{
+    
+    [SerializeField] private UnityEvent eventAfterTime;
+    [SerializeField] private float time;
+    
+    void Start()
+    {
+        StartCoroutine(WaitForEvent());
+    }
+
+    private IEnumerator WaitForEvent()
+    {
+        yield return new WaitForSeconds(time);
+        eventAfterTime.Invoke();
+    }
+}
