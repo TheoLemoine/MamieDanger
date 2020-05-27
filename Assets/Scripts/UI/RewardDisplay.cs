77a98f15a2dfeed1c2e2215b8f45e37008bdc10c
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-05-27 18:22:54 +0200

    :sparkles: :lipstick: Reworked end level event system and animated rewards

---
+++ Assets/Scripts/UI/RewardDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RewardDisplay : MonoBehaviour
+{
+    [SerializeField] private UIScale[] rewards;
+    [SerializeField][Range(0.01f, 1f)] private float offset = 0.2f;
+
+    public void AnimateRewards()
+    {
+        var i = 0;
+        foreach (var reward in rewards)
+        {
+            StartCoroutine(WaitForAnimate(offset * i, reward));
+            i++;
+        }
+    }
+
+    private IEnumerator WaitForAnimate(float seconds, UIScale reward)
+    {
+        yield return new WaitForSeconds(seconds);
+        reward.ScaleIn();
+    }
+}
