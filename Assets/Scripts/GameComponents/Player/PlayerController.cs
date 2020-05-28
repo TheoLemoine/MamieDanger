57edf4a69549a0b16db5ea1afc980359a3106c81
Theo <theo.lemoine0@gmail.com>
2020-05-28 16:08:17 +0200

    :sparkles: Added animated played (broken), and centered scene 1

--- Assets/Scripts/GameComponents/Player/PlayerController.cs
+++ Assets/Scripts/GameComponents/Player/PlayerController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using Global.Input;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
 using Utils;
+using Random = UnityEngine.Random;
 
 namespace GameComponents.Player
 {
@@ -15,8 +17,16 @@ namespace GameComponents.Player
     {
         [SerializeField] private SingleUnityLayer groundLayer;
         [SerializeField] private ChangeDestinationEvent changeDestinationEvent;
+        [SerializeField] private Animator animator;
+        
         private NavMeshAgent _agent;
         private Transform _transform;
+
+        private float _randomThisSecond;
+        
+        private static readonly int ScratchHeadHash = Animator.StringToHash("ScratchHead");
+        private static readonly int LookAroundHash = Animator.StringToHash("LookAround");
+        private static readonly int SpeedHash = Animator.StringToHash("Speed");
 
         private void Start()
         {
@@ -26,6 +36,30 @@ namespace GameComponents.Player
             _transform = transform;
             _agent = GetComponent<NavMeshAgent>();
             ListenInputs();
+
+            StartCoroutine(UpdateRandom());
+        }
+
+        private IEnumerator UpdateRandom()
+        {
+            for (;;)
+            {
+                _randomThisSecond = Random.Range(0f, 1f);
+                yield return new WaitForSeconds(1f);
+            }
+        }
+
+        private void Update()
+        {
+            animator.ResetTrigger(ScratchHeadHash);
+            animator.ResetTrigger(LookAroundHash);
+            
+            if (0.8 <= _randomThisSecond && _randomThisSecond <= 0.9)
+                animator.SetTrigger(ScratchHeadHash);
+            else if (0.9 <= _randomThisSecond && _randomThisSecond <= 1)
+                animator.SetTrigger(LookAroundHash);
+            
+            animator.SetFloat(SpeedHash, _agent.velocity.magnitude);
         }
 
         private void OnDestroy()
