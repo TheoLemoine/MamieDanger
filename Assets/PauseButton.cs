c3b0915c904a74ce8e3965832626e4a3d7e99520
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-06-04 01:05:39 +0200

    :recycle: Animated and moved pause button out of pause menu

---
+++ Assets/PauseButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class PauseButton : MonoBehaviour
+{
+    [SerializeField][Range(0.1f, 1f)] private float duration = 0.4f;
+    [SerializeField] private RectTransform pauseBarTransform1;
+    [SerializeField] private RectTransform pauseBarTransform2;
+    [SerializeField] private Pause pause;
+
+    private bool _isCross;
+
+    public void Toggle()
+    {
+        if (_isCross)
+        {
+            TransitionToPauseSymbol();
+            pause.ResumeToGame();
+        }
+        else
+        {
+            TransitionToCross();
+            pause.PauseGame();
+        }
+        _isCross = !_isCross;
+    }
+
+
+    void TransitionToCross()
+    {
+        pauseBarTransform1.DOLocalRotate(new Vector3(0, 0, -45), duration).SetUpdate(true);
+        pauseBarTransform1.DOLocalMove(new Vector3(-2.95f, 5.8f, 0), duration).SetUpdate(true);
+        pauseBarTransform1.DOScaleY(1.213f, duration).SetUpdate(true);
+        pauseBarTransform2.DOLocalRotate(new Vector3(0, 0, 45), duration).SetUpdate(true);
+        pauseBarTransform2.DOLocalMove(new Vector3(10.28f, 5.8f, 0), duration).SetUpdate(true);
+        pauseBarTransform2.DOScaleY(1.213f, duration).SetUpdate(true);
+    }
+
+    void TransitionToPauseSymbol()
+    {
+        pauseBarTransform1.DOLocalRotate(new Vector3(0, 0, 0), duration).SetUpdate(true);
+        pauseBarTransform1.DOLocalMove(new Vector3(-4.8487f, 5.6177f, 0), duration).SetUpdate(true);
+        pauseBarTransform1.DOScaleY(1, duration).SetUpdate(true);
+        pauseBarTransform2.DOLocalRotate(new Vector3(0, 0, 0), duration).SetUpdate(true);
+        pauseBarTransform2.DOLocalMove(new Vector3(11.634f, 4.9147f, 0), duration).SetUpdate(true);
+        pauseBarTransform2.DOScaleY(1, duration).SetUpdate(true);
+    }
+}
