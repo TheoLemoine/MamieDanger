ba352ac07a83bb60f6594207e0c6d89b867b631d
LeonBaudouin <leondedouin@gmail.com>
2020-06-09 11:27:26 +0200

    :speech_balloon: Updated dialogs and added triggers on Level1

--- Assets/Scripts/Terrain/OnceZoneTrigger.cs
+++ Assets/Scripts/Terrain/OnceZoneTrigger.cs
@@ -5,7 +5,7 @@ public class OnceZoneTrigger : ZoneTrigger
     private bool _hasBeenTriggered;
 
     private void OnTriggerEnter(Collider other)
-    {
+    {    
         if (!_hasBeenTriggered && other.CompareTag(targetTag))
         {
             triggerEvent.Invoke();
