e53a6c3717adca5b5efc588eac3df45d442fe354
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-06-10 17:15:44 +0200

    :bug: Fixed flicker on Play button

--- Assets/Scripts/Utils/TimeMaterial.cs
+++ Assets/Scripts/Utils/TimeMaterial.cs
@@ -7,6 +7,8 @@ public class TimeMaterial : MonoBehaviour
 {
     [SerializeField] private string durationPropCode = "_duration";
     [SerializeField] private string startTimePropCode = "_startTime";
+    [SerializeField] private bool doesResetTime;
+    [SerializeField] private float resetTime;
     private Material _material;
     private float _duration;
     private int _startTimePropId;
@@ -18,10 +20,17 @@ public class TimeMaterial : MonoBehaviour
         
         _startTimePropId = Shader.PropertyToID(startTimePropCode);
         _duration = _material.GetFloat(Shader.PropertyToID(durationPropCode));
+        if (doesResetTime)
+            SetTime(resetTime);
     }
 
     public void TriggerTime()
     {
         _material.SetFloat(_startTimePropId, Time.time);
+    }
+
+    public void SetTime(float time)
+    {
+        _material.SetFloat(_startTimePropId, time);
     }
 }
