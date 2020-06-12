5a0456f69ca148dd8422c76c7cec234574f4c573
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-06-12 11:25:16 +0200

    :truck: Moved time material to Utils namespace

--- Assets/Scripts/Utils/TimeMaterial.cs
+++ Assets/Scripts/Utils/TimeMaterial.cs
@@ -1,36 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class TimeMaterial : MonoBehaviour
+namespace Utils
 {
-    [SerializeField] private string durationPropCode = "_duration";
-    [SerializeField] private string startTimePropCode = "_startTime";
-    [SerializeField] private bool doesResetTime;
-    [SerializeField] private float resetTime;
-    private Material _material;
-    private float _duration;
-    private int _startTimePropId;
+    public class TimeMaterial : MonoBehaviour
+    {
+        [SerializeField] private string durationPropCode = "_duration";
+        [SerializeField] private string startTimePropCode = "_startTime";
+        [SerializeField] private bool doesResetTime;
+        [SerializeField] private float resetTime;
+        private Material _material;
+        private float _duration;
+        private int _startTimePropId;
     
-    void Start()
-    {
-        var meshRenderer = GetComponent<MeshRenderer>();
-        _material = meshRenderer != null ? meshRenderer.material : GetComponent<Image>().material;
+        void Start()
+        {
+            var meshRenderer = GetComponent<MeshRenderer>();
+            _material = meshRenderer != null ? meshRenderer.material : GetComponent<Image>().material;
         
-        _startTimePropId = Shader.PropertyToID(startTimePropCode);
-        _duration = _material.GetFloat(Shader.PropertyToID(durationPropCode));
-        if (doesResetTime)
-            SetTime(resetTime);
-    }
+            _startTimePropId = Shader.PropertyToID(startTimePropCode);
+            _duration = _material.GetFloat(Shader.PropertyToID(durationPropCode));
+            if (doesResetTime)
+                SetTime(resetTime);
+        }
 
-    public void TriggerTime()
-    {
-        _material.SetFloat(_startTimePropId, Time.time);
-    }
+        public void TriggerTime()
+        {
+            _material.SetFloat(_startTimePropId, Time.time);
+        }
 
-    public void SetTime(float time)
-    {
-        _material.SetFloat(_startTimePropId, time);
+        public void SetTime(float time)
+        {
+            _material.SetFloat(_startTimePropId, time);
+        }
     }
 }
